70474758cd2795681bdc94ab059365e3a664b3b7
Mark Mnei <MarkMnei@gmail.com>
2016-03-19 17:44:53 -0400

    "Added port for Heroku"

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app=express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 //important to put middleware at the top
 var middleware = require('./middleware.js');
