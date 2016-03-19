e9fd9267f22ecc39e1d0ff86adbedb5f542bd0e7
Mark Mnei <MarkMnei@gmail.com>
2016-03-19 16:43:44 -0400

    Inilialize Repo

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app=express();
+var PORT = 3000;
+
+//important to put middleware at the top
+var middleware = require('./middleware.js');
+//
+app.use(middleware.logger);
+app.use(middleware.requireAuthentication);
+
+//
+//can impose middle at action level
+//app.get('/about',middleware.requireAuthentication, function(req,res){
+//    res.send('about Us!!')
+//});
+
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function(){
+    console.log('Express server stated on port '  + PORT);
+});
